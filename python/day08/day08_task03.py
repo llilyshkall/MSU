2090828d9624b8109a96c6ea60c761fc1c3fdf9e
Илья Супрун <llilyshkall@MacBook-Pro-Ila.local>
2023-11-05 23:28:02 +0300

    fix style

--- python/day08/day08_task03.py
+++ python/day08/day08_task03.py
@@ -1,14 +1,14 @@
 
 
 class Geom:
-    def __init__(self, base, deno, start = 0, end = None) -> None:
+    def __init__(self, base, deno, start=0, end=None) -> None:
         self.base = base
         self.deno = deno
         self.start = start
         self.end = end
-    
+
     def __getitem__(self, index):
-        if index == ... or type(index) == tuple:
+        if index == ... or type(index) is tuple:
             start = 0
             end = None
             if index != ...:
@@ -20,17 +20,20 @@ class Geom:
                     end = index[1] if index[0] == ... else None
             return Geom(self.base, self.deno, start, end)
         elif type(index) is slice:
-            if index.start is None and index.stop is None and index.step is None:
+            if index.start is None and \
+               index.stop is None and \
+               index.step is None:
                 return Geom(self.base, self.deno)
-            ret = [self[i] for i in range(index.start or 0, index.stop or -1, index.step or 1)]
+            ret = [self[i] for i in
+                   range(index.start or 0, index.stop or -1, index.step or 1)]
             return ret
         else:
             return self.base * (self.deno ** index)
-    
+
     def __iter__(self):
         self.n = self.start
         return self
-    
+
     def __next__(self):
         if self.n >= 0 and (self.end is None or self.n < self.end):
             ret = self.base * (self.deno ** self.n)
@@ -38,10 +41,3 @@ class Geom:
             return ret
         else:
             raise StopIteration
-
-
-# g = Geom(3, 2)
-# print(*zip("012345", g[::]))
-# print(*zip("012345", g[:]))
-# print(*g[..., 11])
-# print(*zip(g[20, ...], "0123"))
